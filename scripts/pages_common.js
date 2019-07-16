639f46c2ee991ea6ff66016a2eb0d3da69f27850
Paulius Bieksa <priverstinis@gmail.com>
2019-07-16 19:43:14 +0100

    Changed the layout of main page. Started GPU parallelisation article page. Made a small script to add correspoding tags to all article pages.

---
+++ scripts/pages_common.js
@@ -0,0 +1,25 @@
+let card_contents = JSON.parse(data);
+
+// Add tags to title section
+let title_container = document.getElementsByClassName('article_title')[0];
+let title_element = document.getElementById('page_title');
+let title = title_element.textContent;
+for (single of card_contents)
+{
+	console.log(single['title']);
+	console.log(title);
+	if (single['title'] == title)
+	{
+		for (tag in single['tags'])
+		{
+			console.log('if');
+			let tag_element = document.createElement("a");
+			tag_element.textContent = single['tags'][tag];
+			tag_element.classList.add('tag');
+			let p = document.getElementById('date_stamp');
+			title_container.insertBefore(tag_element, p);
+			//title_container.appendChild(tag_element);
+		}
+		break;
+	}
+}
